ce786c65b6d481fc40244ce801a9ee25618d555e
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2018-02-17 00:27:18 -0500

    Simplify open example

--- examples/open.ts
+++ examples/open.ts
@@ -1,18 +1,12 @@
 import nano from './init'
 
 export default async function open() {
-  try {
-    //create a new key
-    const target = await nano.key.create()
+  //create a new key
+  const target = await nano.key.create()
 
-    //open block with send's hash
-    const account = nano.withAccount(process.env.SENDER_WALLET_PRIVATE_KEY)
-    const result = await account.open()
-
-    return {result, target}
-  } catch (err) {
-    throw new Error(err.message)
-  }
+  //open block with send's hash
+  const account = nano.withAccount(process.env.SENDER_WALLET_PRIVATE_KEY)
+  await account.open()
 }
 
 open()
