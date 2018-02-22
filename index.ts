84251c7451aa5867d80d106b68b97b8daaffa474
Arman Dezfuli-Arjomandi <rawrmaan@gmail.com>
2018-02-22 01:25:02 -0500

    Dont mark functions async if they dont await anything

--- index.ts
+++ index.ts
@@ -55,10 +55,10 @@ export default class Nano {
       this.rpc = createAPI<API>(rpcClient)
     }
 
-    this.log = this.log.bind(this)
+    this._log = this._log.bind(this)
   }
 
-  log(message: string) {
+  _log(message: string) {
     if (this.debug) {
       console.log(message)
     }
@@ -116,7 +116,7 @@ export default class Nano {
     representative?: string,
     sendBlockHash?: string
   ) {
-    const {log} = this
+    const {_log} = this
 
     if (!privateKey) {
       throw new Error('Must pass privateKey argument')
@@ -148,7 +148,7 @@ export default class Nano {
     })
 
     const result = await this.blocks.publish(block.block)
-    log(
+    _log(
       `Opened NANO account ${address} with block ${
         result.hash
       } and representative ${representative}!`
@@ -158,7 +158,7 @@ export default class Nano {
 
   //Top-level call: send block
   async send(privateKey: string, amount: string | number, toAddress: string) {
-    const {log} = this
+    const {_log} = this
 
     if (!privateKey) {
       throw new Error('Must pass private_key argument')
@@ -169,7 +169,6 @@ export default class Nano {
 
     const block = await this.blocks.createSend({
       key: privateKey,
-      // account: address,
       destination: toAddress,
       balance,
       amount: amountRaw,
@@ -178,13 +177,13 @@ export default class Nano {
     })
 
     const result = await this.blocks.publish(block.block)
-    log(`Sent ${amountRaw} NANO to ${toAddress}!`)
+    _log(`Sent ${amountRaw} NANO to ${toAddress}!`)
     return result.hash
   }
 
   //Top-level call: receive block
   async receive(privateKey: string, sendBlockHash?: string) {
-    const {log} = this
+    const {_log} = this
 
     if (!privateKey) {
       throw new Error('Must pass privateKey argument')
@@ -209,13 +208,13 @@ export default class Nano {
     })
 
     const result = await this.blocks.publish(block.block)
-    log(`Received block ${sendBlockHash} to wallet ${address}!`)
+    _log(`Received block ${sendBlockHash} to wallet ${address}!`)
     return result
   }
 
   //Top-level call: change block
   async change(privateKey: string, representative: string) {
-    const {log} = this
+    const {_log} = this
 
     if (!privateKey) {
       throw new Error('Must pass privateKey argument')
@@ -231,7 +230,7 @@ export default class Nano {
     })
 
     const result = await this.blocks.publish(block.block)
-    log(`Opened NANO block ${result.hash} with rep. ${representative}!`)
+    _log(`Opened NANO block ${result.hash} with rep. ${representative}!`)
     return result
   }
 
@@ -250,45 +249,45 @@ export default class Nano {
 
   //General account methods
   get accounts() {
-    const {rpc, log} = this
+    const {rpc, _log} = this
     return {
-      async get(publicKey: string) {
+      get(publicKey: string) {
         return rpc('account_get', {key: publicKey})
       },
-      async rawBalance(account: string) {
+      rawBalance(account: string) {
         return rpc('account_balance', {account})
       },
-      async nanoBalance(account: string) {
+      nanoBalance(account: string) {
         return rpc('account_balance', {account}).then(balance =>
           Converter.unit(balance.balance, 'raw', 'NANO')
         )
       },
-      async balances(accounts: string[]) {
+      balances(accounts: string[]) {
         return rpc('accounts_balances', {accounts})
       },
-      async blockCount(account: string) {
+      blockCount(account: string) {
         return rpc('account_block_count', {account})
       },
-      async frontiers(accounts: string[]) {
+      frontiers(accounts: string[]) {
         return rpc('accounts_frontiers', {accounts})
       },
-      async history(account: string, count?: number) {
+      history(account: string, count?: number) {
         return rpc('account_history', {
           account,
           count: count || 1000
         }).then(res => res.history)
       },
-      async info(account: string) {
+      info(account: string) {
         return rpc('account_info', {account}).then(account => {
-          log(`(ACCOUNT) balance: ${account.balance}`)
-          log(`(ACCOUNT) latest hash: ${account.frontier}`)
+          _log(`(ACCOUNT) balance: ${account.balance}`)
+          _log(`(ACCOUNT) latest hash: ${account.frontier}`)
           return account
         })
       },
-      async key(account: string) {
+      key(account: string) {
         return rpc('account_key', {account})
       },
-      async ledger(account: string, count?: number, details?: boolean) {
+      ledger(account: string, count?: number, details?: boolean) {
         return rpc('ledger', {
           account,
           count: count || 1000,
@@ -297,19 +296,18 @@ export default class Nano {
           pending: details
         })
       },
-      async pending(
+      pending(
         account: string,
         count?: number,
         minNanoThreshold?: string | number
       ) {
-        // TODO: convert threshold from xrb to raw
         return rpc('pending', {
           account,
           threshold: Converter.unit(minNanoThreshold || 0, 'NANO', 'raw'),
           count: count || 1000
         })
       },
-      async pendingMulti(
+      pendingMulti(
         accounts: string[],
         count?: number,
         minNanoThreshold?: string | number
@@ -320,52 +318,50 @@ export default class Nano {
           count: count || 1000
         })
       },
-      async representative(account: string) {
+      representative(account: string) {
         return rpc('account_representative', {
           account
         }).then(res => res.representative)
       },
-      async weight(account: string) {
+      weight(account: string) {
         return rpc('account_weight', {account}).then(res => res.weight)
       }
     }
   }
 
   get blocks() {
-    const {rpc, log} = this
+    const {rpc, _log} = this
 
     return {
-      async account(hash: string) {
-        return rpc('block_account', {hash}).then(res => {
-          return res.account
-        })
+      account(hash: string) {
+        return rpc('block_account', {hash}).then(res => res.account)
       },
-      async count(by_type?: boolean) {
-        return by_type ? rpc('block_count_type') : rpc('block_count')
+      count(byType?: boolean) {
+        return byType ? rpc('block_count_type') : rpc('block_count')
       },
-      async chain(block: string, count?: number) {
+      chain(block: string, count?: number) {
         return rpc('chain', {
           block,
           count: count || 1000
         }).then(res => res.blocks)
       },
-      async createChange(block: ChangeBlock) {
+      createChange(block: ChangeBlock) {
         return rpc('block_create', {
           type: 'change',
           ...block
         }).then(res => {
-          log(`(BLOCK) Changing ${block.key}`)
+          _log(`(BLOCK) Changing ${block.key}`)
           return res
         })
       },
-      async history(hash: string, count?: number) {
+      history(hash: string, count?: number) {
         return rpc('history', {
           hash,
           count: count || 1000
         })
       },
       //Get one or many block's information
-      async info(hashOrHashes: string | string[], details?: boolean) {
+      info(hashOrHashes: string | string[], details?: boolean) {
         const getMulti = (typeof hashOrHashes as string | string[]) === 'array'
         if (getMulti) {
           return details
@@ -381,43 +377,43 @@ export default class Nano {
           }).then(res => res.contents)
         }
       },
-      async createOpen(block: OpenBlock) {
+      createOpen(block: OpenBlock) {
         return rpc('block_create', {
           type: 'open',
           ...block
         }).then(res => {
-          log(`(BLOCK) Opening ${block.key}`)
+          _log(`(BLOCK) Opening ${block.key}`)
           return res
         })
       },
-      async pending(hash: string) {
+      pending(hash: string) {
         return rpc('pending_exists', {hash}).then(res => res.exists === '1')
       },
-      async publish(block: string) {
+      publish(block: string) {
         return rpc('process', {block: block}).then(res => {
-          log(`(BLOCK) Published: ${res.hash}`)
+          _log(`(BLOCK) Published: ${res.hash}`)
           return res
         })
       },
-      async createReceive(block: ReceiveBlock) {
+      createReceive(block: ReceiveBlock) {
         return rpc('block_create', {
           type: 'receive',
           ...block
         }).then(res => {
-          log(`Received block ${block.source}`)
+          _log(`Received block ${block.source}`)
           return res
         })
       },
-      async createSend(block: SendBlock) {
+      createSend(block: SendBlock) {
         return rpc('block_create', {
           type: 'send',
           ...block
         }).then(res => {
-          log(`(BLOCK) Sending ${block.amount} to ${block.destination}`)
+          _log(`(BLOCK) Sending ${block.amount} to ${block.destination}`)
           return res
         })
       },
-      async successors(block: string, count?: number) {
+      successors(block: string, count?: number) {
         return rpc('successors', {
           block,
           count: count || 1000
@@ -479,12 +475,12 @@ export default class Nano {
 
   //Generate and get work
   get work() {
-    const {rpc, log} = this
+    const {rpc, _log} = this
 
     return {
       generate(hash: string) {
         return rpc('work_generate', {hash}).then(result => {
-          log(`(WORK) generated PoW: ${result.work}`)
+          _log(`(WORK) generated PoW: ${result.work}`)
           return result
         })
       },
